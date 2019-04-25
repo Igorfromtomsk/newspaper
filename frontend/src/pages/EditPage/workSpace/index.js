5d2ed3c3bc0c0304b73ff257e054e5e9142ddbc7
Igor Lednev <igor.lednev@quantumsoft.ru>
2019-04-25 12:22:38 +0700

    drawing works fine

--- frontend/src/pages/EditPage/workSpace/index.js
+++ frontend/src/pages/EditPage/workSpace/index.js
@@ -10,17 +10,33 @@ class WorkSpaceComp extends Component {
     super(props);
 
     this.workSpaceInner = React.createRef();
-    this.state = {drawingIsStarted: false}
+    this.state = {drawingIsStarted: false};
+  }
+
+  getCoordinate(e) {
+    let {width} = this.workSpaceInner.current.getBoundingClientRect();
+    let {leftPadding} = this.props;
+    const relativeXCoordinate = this.workSpaceInner.current === e.target;
+    const xCoordinate = relativeXCoordinate ?
+      e.nativeEvent.offsetX :
+      leftPadding >= e.nativeEvent.offsetX ?
+        (leftPadding - e.nativeEvent.offsetX) * -1 :
+        e.nativeEvent.offsetX - leftPadding;
+
+    return 100 * xCoordinate / width;
   }
 
   startDrawing(e) {
     let {layers} = this.props;
-    let {width} = this.workSpaceInner.current.getBoundingClientRect();
 
     let newLayer = {
       id: layers.length,
-      left: 100 * e.nativeEvent.offsetX / width,
-      top: e.nativeEvent.offsetY,
+      coords: {
+        x1: this.getCoordinate(e),
+        y1: e.nativeEvent.offsetY,
+        x2: 0,
+        y2: 0
+      },
       width: 0,
       height: 0
     };
@@ -38,12 +54,12 @@ class WorkSpaceComp extends Component {
     e.stopPropagation();
 
     let {activeLayerId, layers} = this.props;
-    let {width} = this.workSpaceInner.current.getBoundingClientRect();
-
     let activeLayer = layers.filter(layer => layer.id === activeLayerId)[0];
 
-    activeLayer.width = 100 * e.nativeEvent.offsetX / width - activeLayer.left;
-    activeLayer.height = e.nativeEvent.offsetY - activeLayer.top;
+    activeLayer.coords.x2 = this.getCoordinate(e);
+    activeLayer.coords.y2 = e.nativeEvent.offsetY;
+    activeLayer.width = Math.abs(this.getCoordinate(e) - activeLayer.coords.x1);
+    activeLayer.height = Math.abs(e.nativeEvent.offsetY - activeLayer.coords.y1);
 
     this.setState({layers: [...layers]});
   }
@@ -72,8 +88,8 @@ class WorkSpaceComp extends Component {
             className={S.rectangle}
             key={layer.id}
             style={{
-              left: `${layer.left}%`,
-              top: `${layer.top}px`,
+              left: `${Math.abs(layer.coords.x1) <= Math.abs(layer.coords.x2) ? layer.coords.x1 : layer.coords.x2}%`,
+              top: `${layer.coords.y1 <= layer.coords.y2 ? layer.coords.y1 : layer.coords.y2}px`,
               width: `${layer.width}%`,
               height: `${layer.height}px`,
               pointerEvents: drawingIsStarted ? 'none' : 'auto'
